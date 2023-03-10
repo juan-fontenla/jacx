320fd532327d3d173fca80ccf17c2aa44321e1f7
anamarialc <ana.maria.lema.carril@gmail.com>
2023-03-10 17:16:28 +0100

    Implementa menu para la ruta de viajes

---
+++ app/src/main/java/com/apm/jacx/ItineraryActivity.kt
@@ -0,0 +1,11 @@
+package com.apm.jacx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItineraryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_itinerary)
+    }
+}
