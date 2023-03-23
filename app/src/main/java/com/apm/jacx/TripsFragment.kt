6d80f2621cfa808a5f35fbac50375bd4441e93cc
Juan Fontenla <juan.fontenla@udc.es>
2023-03-23 01:06:23 +0100

    refactor: Add sample data to planned trips view

--- app/src/main/java/com/apm/jacx/TripsFragment.kt
+++ app/src/main/java/com/apm/jacx/TripsFragment.kt
@@ -2,10 +2,15 @@ package com.apm.jacx
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.apm.jacx.adapter.ItemTripAdapter
+import com.apm.jacx.data.Datasource
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 // TODO: Rename parameter arguments, choose names that match
@@ -46,7 +51,9 @@ class TripsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trips, container, false)
+        val viewFragment = inflater.inflate(R.layout.fragment_trips, container, false)
+        loadTripsFragmentData(viewFragment)
+        return viewFragment
     }
 
     private fun onJoinButtonClick() {
@@ -81,5 +88,20 @@ class TripsFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    private fun loadTripsFragmentData(viewFragment: View ) {
+        // Initialize data.
+        val myDataset = Datasource().loadTrips()
+
+        Log.d("Trips dataset loaded", myDataset.toString())
+
+        val recyclerView = viewFragment.findViewById<RecyclerView>(R.id.list_own_trips)
+        recyclerView?.adapter = context?.let { ItemTripAdapter(it, myDataset) }
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        // recyclerView.setHasFixedSize(true)
+
     }
 }
