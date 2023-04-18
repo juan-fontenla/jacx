d8ae416e36090fb892b5fd4c6952207c7bd34673
clarajuncal1 <clara.juncal@udc.es>
2023-04-18 19:25:45 +0200

    anadido mapa

--- app/src/main/java/com/apm/jacx/DetailRouteActivity.kt
+++ app/src/main/java/com/apm/jacx/DetailRouteActivity.kt
@@ -1,19 +1,20 @@
 package com.apm.jacx
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.Toast
+import android.util.Log
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.OnMapsSdkInitializedCallback
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 
-class DetailRouteActivity : AppCompatActivity() {
+class DetailRouteActivity : AppCompatActivity(), OnMapsSdkInitializedCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_route)
+
+        MapsInitializer.initialize(applicationContext, MapsInitializer.Renderer.LATEST, this)
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation_trip)
         bottomNav.setOnItemSelectedListener(navListener)
@@ -37,5 +38,12 @@ class DetailRouteActivity : AppCompatActivity() {
         }
         supportFragmentManager.beginTransaction().replace(R.id.main_view_container_trip, selectedFragment).commit()
         true
+    }
+
+    override fun onMapsSdkInitialized(renderer: MapsInitializer.Renderer) {
+        when (renderer) {
+            MapsInitializer.Renderer.LATEST -> Log.d("MapsDemo", "The latest version of the renderer is used.")
+            MapsInitializer.Renderer.LEGACY -> Log.d("MapsDemo", "The legacy version of the renderer is used.")
+        }
     }
 }
---
+++ app/src/main/java/com/apm/jacx/MapFragment.kt
@@ -0,0 +1,46 @@
+package com.apm.jacx
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MapFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_map, container, false)
+        // Obtain the map fragment
+        val mapFragment: SupportMapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        // Async map
+        mapFragment.getMapAsync(this)
+        return view
+    }
+
+
+   override fun onMapReady(googleMap: GoogleMap) {
+       mMap = googleMap
+   }
+}
