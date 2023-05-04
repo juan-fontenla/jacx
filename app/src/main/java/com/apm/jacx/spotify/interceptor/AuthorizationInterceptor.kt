9292fe637c6412d501dd729744b76fd198ddf6db
Xaime Pardal <xaime_pardal@outlook.com>
2023-05-04 17:04:45 +0200

    almacenamiento interno OK

--- app/src/main/java/com/apm/jacx/spotify/interceptor/AuthorizationInterceptor.kt
+++ app/src/main/java/com/apm/jacx/spotify/interceptor/AuthorizationInterceptor.kt
@@ -1,17 +1,24 @@
 package com.apm.jacx.spotify.interceptor
-import com.apm.jacx.MainApplication
+
+import com.apm.jacx.internalStorage.AppPreferences
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class AuthorizationInterceptor : Interceptor {
 
+    private val token = AppPreferences.TOKEN_SPOTIFY;
+
+  /*  var prefs = PreferenceManager.getDefaultSharedPreferences(Context.);
+    prefs = prefs.getSharedPreferences(Resources.getSystem().getString(),
+        Context.MODE_PRIVATE
+    );*/
     // Para cada peticion se añade en el header de cada peticion
     override fun intercept(chain: Interceptor.Chain): Response =
         chain.proceed(
             chain
                 .request()
                 .newBuilder()
-                .addHeader("Authorization", "Bearer ${MainApplication.TOKEN_SPOTIFY}")
+                .addHeader("Authorization", "Bearer $token")
                 .build()
         )
 }
