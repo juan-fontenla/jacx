2709e89bf38de84a529ccd7ef6ff27752e278816
Juan Fontenla <juan.fontenla@udc.es>
2023-05-10 02:02:50 +0200

    feat: Added autocomplete directions component to trip form

--- app/src/main/java/com/apm/jacx/ItineraryFragment.kt
+++ app/src/main/java/com/apm/jacx/ItineraryFragment.kt
@@ -12,11 +12,15 @@ import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.widget.Autocomplete
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -90,7 +94,7 @@ class ItineraryFragment : Fragment() {
 
         val btnAddRoute : FloatingActionButton = viewFragment.findViewById(R.id.btn_add_route_trip_friend)
         btnAddRoute.setOnClickListener {
-            val fields = listOf(Place.Field.ID, Place.Field.NAME)
+            val fields = listOf(Place.Field.LAT_LNG, Place.Field.NAME)
             val intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields)
                 .build(activity)
             startAutocomplete.launch(intent)
@@ -102,18 +106,28 @@ class ItineraryFragment : Fragment() {
     private val startAutocomplete =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
             if (result.resultCode == Activity.RESULT_OK) {
+                // Process selected waypoint
                 val intent = result.data
                 if (intent != null) {
                     val place = Autocomplete.getPlaceFromIntent(intent)
-                    Log.i(
-                        "ItineraryFragment", "Place: ${place.name}, ${place.id}"
-                    )
+                    sendWaypoint(place.latLng)
                 }
             } else if (result.resultCode == Activity.RESULT_CANCELED) {
                 // The user canceled the operation.
                 Log.i("ItineraryFragment", "User canceled autocomplete")
             }
         }
+
+    private fun sendWaypoint(latLng: LatLng) = runBlocking {
+        Log.d("ItineraryFragment", "SEND WAYPOINT!")
+        val pointString = "${latLng.latitude},${latLng.longitude}"
+        // TODO send point to server (maybe include name)
+
+        // TODO add to list if successful
+        activity?.runOnUiThread {
+            Toast.makeText(activity, "Send $pointString waypoint to server", Toast.LENGTH_LONG).show()
+        }
+    }
 
 
     private fun saveRoute(viewFragment: View) {
